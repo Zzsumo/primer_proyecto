053df3c1d380f5c68fbbd58c9929d1d9583b4b77
Lautaro Ramos <lautaroramos0212@gmail.com>
2024-10-07 11:05:12 -0300

    agregando js

---
+++ scripts/app.js
@@ -0,0 +1,6 @@
+let miDato = prompt("Looking for shirts?");
+if (miDato === "yes" || miDato === "YES" || miDato === "") {
+  alert("Welcome to BEHERIT!");
+} else {
+  alert("Come back another day :(");
+}
